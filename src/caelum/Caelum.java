47962d17f52c537261b104874a2debf678d050e3
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2015-08-09 19:44:52 -0300

    Estudo sobre classes e tipos primitivos

--- src/caelum/Caelum.java
+++ src/caelum/Caelum.java
@@ -5,10 +5,7 @@ package caelum;
  * @author Michael
  */
 public class Caelum {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Michael");
     }
